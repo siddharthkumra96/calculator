3e1bb6459cc4b92c001d00638e2129318415a37c
siddharthkumra@vinsol.com <siddharthkumra@vinsol.com>
2019-11-18 13:07:54 +0530

    Updates

---
+++ precache-manifest.d62dc39061f61872e3b3409f1e499c41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb031f330ccbc2689c61d82544ca4ac",
+    "url": "/siddharthkumra96/calculator/index.html"
+  },
+  {
+    "revision": "a8090cd5bb1c9fa43d3b",
+    "url": "/siddharthkumra96/calculator/static/css/main.dc4ffe9a.chunk.css"
+  },
+  {
+    "revision": "17c66c50019ce823554f",
+    "url": "/siddharthkumra96/calculator/static/js/2.625c2072.chunk.js"
+  },
+  {
+    "revision": "a8090cd5bb1c9fa43d3b",
+    "url": "/siddharthkumra96/calculator/static/js/main.e91cc32b.chunk.js"
+  },
+  {
+    "revision": "bb62342797d4ae089d27",
+    "url": "/siddharthkumra96/calculator/static/js/runtime-main.a91c36fb.js"
+  }
+]);
